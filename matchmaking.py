83ac04874f369fbf4cd30fe3177fc75da3c54cf4
Joe Singleton <joesingo@gmail.com>
2017-01-08 14:37:12 +0000

    Add initial implementation of flask server and matchmaking system. Users can create a game, join the game, and see the progress as other people join the game

---
+++ matchmaking.py
@@ -0,0 +1,42 @@
+from jte import Game
+
+class InvalidNameException(Exception):
+    """There is a problem with the username provided by the user"""
+
+
+class GameFullException(Exception):
+    """A user tried to join a game that is already fully allocated"""
+
+
+class Matchmaker(object):
+    """An object to facilitate creating a Game object for a game between
+    multiple players who join the game at different times"""
+
+    def __init__(self, no_of_players, game_map):
+        self.no_of_players = no_of_players
+        self.game_map = game_map
+        self.game = None
+
+        # A list of usernames of the user that have joined so far
+        self.player_names = []
+
+    def add_player(self, name):
+        if self.game is not None:
+            raise GameFullException("That game is full")
+
+        if name in self.player_names:
+            raise InvalidNameException("There is already a player with that name")
+
+        self.player_names.append(name)
+
+        if len(self.player_names) == self.no_of_players:
+            self.game = Game(self.game_map, self.player_names)
+
+    def get_status(self):
+        """Return a dictionary containing all status information necessary for a
+        client to know"""
+        return {
+            "ready": (self.game is not None),
+            "player_names": self.player_names,
+            "max_players": self.no_of_players
+        }
---
+++ server.py
@@ -0,0 +1,80 @@
+import random
+import json
+
+from flask import Flask, render_template, request, redirect, abort, session
+
+from matchmaking import Matchmaker, InvalidNameException, GameFullException
+
+app = Flask(__name__)
+app.secret_key = "\x04\xbb5vU\xa1,\xc9\xa3\xa0\x1d\x86\xf7]=}\xe8\xa1\xba\x1b\x18\x9c\x92n"
+
+MAX_GAME_ID = 100
+matchmakers = {}
+
+with open("map.json") as map_file:
+    soton_map = json.load(map_file)
+
+def check_game_exists(game_id):
+    """Check if a game with the specifed ID exists"""
+    if game_id not in matchmakers:
+        abort(404)
+
+@app.route("/create/")
+def create_game():
+    """Render the static page for a user to create a new game"""
+    return render_template("create_game.html")
+
+@app.route("/create/", methods=["POST"])
+def create_game_post():
+    """Handle a POST request from the create game page to actually create the
+    game. Return a redirect to the join page for the newly created game"""
+    while True:
+        game_id = random.randint(1, MAX_GAME_ID)
+
+        if game_id not in matchmakers:
+            break
+
+    try:
+        players = int(request.form["no_of_players"])
+    except ValueError, KeyError:
+        abort(400)
+
+    matchmakers[game_id] = Matchmaker(players, soton_map)
+    return redirect("/join/{}/".format(game_id))
+
+@app.route("/join/<int:game_id>/")
+def join_game(game_id):
+    """Render the page for a user to join a game"""
+    check_game_exists(game_id)
+    return render_template("join_game.html")
+
+@app.route("/join/<int:game_id>/", methods=["POST"])
+def joing_game_post(game_id):
+    """Handle a POST request from the join page to actually add the user to the
+    game"""
+    check_game_exists(game_id)
+
+    try:
+        name = request.form["username"]
+    except KeyError:
+        abort(400)
+
+    try:
+        matchmakers[game_id].add_player(name)
+
+    except (InvalidNameException, GameFullException) as e:
+        return e.message, 400
+
+    session[game_id] = name
+
+    return "", 200
+
+@app.route("/join/<int:game_id>/status/")
+def join_game_status(game_id):
+    """Return the matchmaking status of the specified game as JSON"""
+    check_game_exists(game_id)
+    status = matchmakers[game_id].get_status()
+    return json.dumps(status)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
