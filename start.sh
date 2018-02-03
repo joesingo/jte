55f9801aefadb471d7a7f4c74b0bb21d0571fd09
Joe Singleton <joesingo@gmail.com>
2018-02-03 13:13:58 +0000

    Remove start/stop scripts
    No longer needed as will run with Docker instead

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-socket=/run/lighttpd/jte.socket
-python site.fcgi $socket &
-echo $! > /var/run/jte.pid
-sleep 3
-chown www-data:www-data $socket
-
-./delete_old.sh &
-echo $! > /var/run/jte_delete_old.pid
